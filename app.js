187550ad27d3e524f58c2b8a38c0f67e68fd2a2d
EmirVelazquez <emirleovelazquez@gmail.com>
2020-05-18 22:43:59 -0500

    added helper function to update nav styling. used sass styling to add a class or remove it based on user scrolling

--- app.js
+++ app.js
@@ -1,3 +1,30 @@
 document.addEventListener("DOMContentLoaded", function () {
+    // ==================================================
+    // References to elements or Global variables
+    // ==================================================
+
+    const navBar = document.getElementById("navbar");
     console.log("Hola Amigos!");
+
+    // ==================================================
+    // Main Executions
+    // ==================================================
+
+    colorNav();
+
+    // ==================================================
+    // Helper Functions
+    // ==================================================
+
+    // This function will add a class name to the navbar for display purposes
+    function colorNav() {
+        document.addEventListener("scroll", function () {
+            let distanceScrolled = window.pageYOffset;
+            if (distanceScrolled < 1) {
+                navBar.classList.remove("updateNav");
+            } else {
+                navBar.classList.add("updateNav");
+            }
+        });
+    };
 })
